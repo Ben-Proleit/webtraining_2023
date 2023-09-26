664b350d1345313c35226dfd2cbf625772822e4a
Christoph Gabler <christoph.gabler@proleit.com>
2023-09-26 09:58:36 +0200

    CGB
    prepared template for the next exercise

---
+++ Gabler_Christoph/template/script.js
@@ -0,0 +1,8 @@
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener("DOMContentLoaded", async function(){
+  console.log("Document loaded.");
+
+});
