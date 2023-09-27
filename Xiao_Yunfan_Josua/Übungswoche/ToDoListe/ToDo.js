9b1b62cf9643f55663d4bba153047757c76a8872
Josua Xiao <Yunfan.xiao@proleit.com>
2023-09-27 13:31:25 +0200

    Löschen funzt

--- Xiao_Yunfan_Josua/Übungswoche/ToDoListe/ToDo.js
+++ Xiao_Yunfan_Josua/Übungswoche/ToDoListe/ToDo.js
@@ -1,35 +1,38 @@
-var ToDoListe = new Array();
 var ListID = 0;
 
 function einträgeHinzufügen() {
-  ToDoListe = document.getElementById("ToDo");
-  console.log(ToDoListe);
+  // get todo div, add a Textbox div, set div id,
+  let ToDoListe = document.getElementById("ToDo");
   const div = ToDoListe.appendChild(document.createElement("div"));
   div.setAttribute("id", ListID);
 
+  //create area and buttons
   div.appendChild(document.createElement("textarea"));
   const edit = div.appendChild(document.createElement("button"));
   const kill = div.appendChild(document.createElement("button"));
   const completed = div.appendChild(document.createElement("button"));
 
+  //set button attributes
+  kill.setAttribute("onclick", "eintragLöschen(" + ListID + ");");
   edit.setAttribute("class", "edit");
   kill.setAttribute("class", "kill");
   completed.setAttribute("class", "completed");
-  ListID++;
 
+  //set button pictures
   edit.appendChild(document.createElement("img"));
   kill.appendChild(document.createElement("img"));
   completed.appendChild(document.createElement("img"));
+
+  //counter
+  ListID++;
 }
 
 function eintragLöschen(id) {
-  console.log(id);
-  //  let deathRow = document.getElementById(id)
-  //  try {
-  //     ToDoListe.removeChild
-  //  }
+  //Holt id des Kindes, Holt body, tötet kind
+  let deathRow = document.getElementById(id);
+  let body = document.getElementById("ToDo");
+  body.removeChild(deathRow);
 }
-// document.getElementsByClassName("kill").onclick = eintragLöschen(id);
 
 function eintragBearbeiten() {
   console.log("Test3");
