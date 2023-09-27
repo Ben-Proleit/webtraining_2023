2c20e4e5835e55fa6ca4cbcb0fe550f2c7e3e07d
Josua Xiao <Yunfan.xiao@proleit.com>
2023-09-27 11:19:46 +0200

    Js Einträge hinzufügen

--- Xiao_Yunfan_Josua/Übungswoche/ToDoListe/ToDo.js
+++ Xiao_Yunfan_Josua/Übungswoche/ToDoListe/ToDo.js
@@ -1,3 +1,34 @@
+var ToDoListe = new Array();
+
 function einträgeHinzufügen() {
   console.log("Test");
+  ToDoListe = document.getElementById("ToDo");
+  console.log(ToDoListe);
+  const div = ToDoListe.appendChild(document.createElement("div"));
+  div.appendChild(document.createElement("textarea"));
+  div
+    .appendChild(document.createElement("button"))
+    .setAttribute("class", "edit");
+  div
+    .appendChild(document.createElement("button"))
+    .setAttribute("class", "kill");
+  div
+    .appendChild(document.createElement("button"))
+    .setAttribute("class", "completed");
+}
+
+function eintragLöschen() {
+  console.log("Test2");
+}
+
+function eintragBearbeiten() {
+  console.log("Test3");
+}
+
+function eintragErledigt() {
+  console.log("Test4");
+}
+
+function eintragNichtErledigt() {
+  console.log("Test5");
 }
