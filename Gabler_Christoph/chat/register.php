60b2c0a44cc1096509bb63511e086a28e4841a75
Christoph Gabler <christoph.gabler@proleit.com>
2023-09-28 09:19:25 +0200

    CGB
    updated login; added register; added licensing information for modular home web sockets

---
+++ Gabler_Christoph/chat/register.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <?php
+        require "./src/standard/head.html";
+        ?>
+        <link rel="stylesheet" type="text/css" href="./style/login.css"/>
+    </head>
+    <body class="lang" data-file="login">
+        <?php
+        //load body
+        require "register.html";
+        //print footer
+        require "./src/standard/footer.html";
+        ?>
+    </body>
+</html>
+<?php
+exit();
