211c5db4ebb696452a4b592f21a1e17c07532682
Josua Xiao <Yunfan.xiao@proleit.com>
2023-10-04 11:05:06 +0200

    Win Count eingefügt, Format

--- Xiao_Yunfan_Josua/Übungswoche/TicTacToe/TTT.js
+++ Xiao_Yunfan_Josua/Übungswoche/TicTacToe/TTT.js
@@ -4,6 +4,9 @@ var newArray = [];
 var X = "X";
 var O = "O";
 var won = false;
+var P1 = 0;
+var P2 = 0;
+
 function switcher() {
   Player = !Player;
   console.log(Player);
@@ -39,7 +42,7 @@ function winCheck() {
   winCheckX();
 }
 function winCheckVer() {
-  let Nr = 0;
+  let Nr = 1;
   do {
     let winString = "";
     winString += newArray[Nr];
@@ -50,7 +53,7 @@ function winCheckVer() {
   } while (Nr < 10);
 }
 function winCheckHor() {
-  let Nr = 0;
+  let Nr = 1;
   do {
     let winString = "";
     winString += newArray[Nr];
@@ -72,19 +75,21 @@ function winCheckX() {
   winString += newArray[7];
   CheckCheck(winString);
 }
-//#endregion
-
 /**
  * @param {string} win
  */
 function CheckCheck(win) {
   if (win == "XXX") {
+    P1++;
     winAusgabe(1);
   } else if (win == "OOO") {
+    P2++;
     winAusgabe(2);
   }
 }
+//#endregion
 
+//#region afterWin
 function winAusgabe(who) {
   let tisch = document.getElementsByTagName("tbody").item(0);
   let newTr = tisch.appendChild(document.createElement("tr"));
@@ -92,9 +97,16 @@ function winAusgabe(who) {
   let trTwo = newTr.appendChild(document.createElement("td"));
   let idk = trOne.appendChild(document.createElement("h2"));
   let reset = trTwo.appendChild(document.createElement("button"));
+  reset.appendChild(document.createElement("img"));
+  if (who == 1) {
+    document.getElementById("sideOne").innerHTML = "<h3>Spieler 1</h3>" + P1;
+  } else if (who == 2) {
+    document.getElementById("sideTwo").innerHTML = "<h3>Spieler 1</h3>" + P2;
+  }
 
   idk.textContent = "Spieler " + who + " hat gewonnen";
   newTr.setAttribute("class", "Winner");
+  reset.setAttribute("class", "resett");
   reset.setAttribute("onclick", "reset()");
   won = true;
 }
@@ -113,3 +125,4 @@ function reset() {
   won = false;
   Player = false;
 }
+//#endregion
